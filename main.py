9a87c045785bb70d2744cb85373cd058085792f0
thomas-kw <65559171+thomas-kw@users.noreply.github.com>
2021-07-02 18:32:51 +0900

    gui done

--- main.py
+++ main.py
@@ -1,5 +1,48 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=200, height=189, bg="white")
+pw_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 95, image=pw_img)
+canvas.grid(row=0, column=1)
+
+# Labels
+
+
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+
+username_label = Label(text="Email/Username")
+username_label.grid(row=2, column=0)
+
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+# Entries
+
+website_input = Entry(width=35)
+website_input.grid(row=1, column=1, columnspan=2)
+
+username_input = Entry(width=35)
+username_input.grid(row=2, column=1, columnspan=2)
+
+password_input = Entry(width=21)
+password_input.grid(row=3, column=1)
+
+# Buttons
+
+password_button = Button(text="Generate Password")
+password_button.grid(row=3, column=2)
+
+add_button = Button(text="Add", width=36)
+add_button.grid(row=4, column=1, columnspan=2)
+
+window.mainloop()
